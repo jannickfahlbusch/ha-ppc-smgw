94d1b1f11ca667b43f90cd46bc5f19663134ee9b
Jannick Stephan Fahlbusch <git@jf-projects.de>
2026-04-10 21:19:42 +0200

    Fix reloading

--- custom_components/ppc_smgw/__init__.py
+++ custom_components/ppc_smgw/__init__.py
@@ -129,8 +129,7 @@ async def async_reload_entry(
     entry: ConfigEntry,
 ) -> None:
     """Reload config entry."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    await hass.config_entries.async_reload(entry.entry_id)
 
 
 async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
